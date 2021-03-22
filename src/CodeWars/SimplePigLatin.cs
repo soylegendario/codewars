18d9f2964d4e09e08d5b8dd167f644d3b8104fdc
Rafael León <rafael.leon@atmira.com>
2021-03-22 10:13:43 +0100

    Add comments to last katas

--- src/CodeWars/SimplePigLatin.cs
+++ src/CodeWars/SimplePigLatin.cs
@@ -1,4 +1,11 @@
-﻿namespace CodeWars
+﻿/*
+ Move the first letter of each word to the end of it, then add "ay" to the end of the word. Leave punctuation marks untouched.
+
+Examples
+Kata.PigIt("Pig latin is cool"); // igPay atinlay siay oolcay
+Kata.PigIt("Hello world !");     // elloHay orldway !
+ */
+namespace CodeWars
 {
     public class SimplePigLatin
     {

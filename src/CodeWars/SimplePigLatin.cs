9fd9a7873b887a90d0fe5f512220f963467bfac7
Rafael León <rafael.leon@atmira.com>
2021-03-22 08:22:39 +0100

    Simple pig latin class

---
+++ src/CodeWars/SimplePigLatin.cs
@@ -0,0 +1,23 @@
+﻿namespace CodeWars
+{
+    public class SimplePigLatin
+    {
+        public static string PigIt(string str)
+        {
+            var splitted = str.Split(" ");
+            str = "";
+            foreach (var word in splitted)
+            {
+                if (word.Length > 1)
+                {
+                    str += $"{word.Substring(1)}{word.Substring(0, 1)}ay ";
+                }
+                else
+                {
+                    str += word;
+                }
+            }
+            return str.Trim();
+        }
+    }
+}
---
+++ src/CodeWarsTests/SimplePigLatinTests.cs
@@ -0,0 +1,16 @@
+﻿using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class SimplePigLatinTests
+    {
+        [Test]
+        public void KataTests()
+        {
+            Assert.AreEqual("igPay atinlay siay oolcay", SimplePigLatin.PigIt("Pig latin is cool"));
+            Assert.AreEqual("hisTay siay ymay tringsay", SimplePigLatin.PigIt("This is my string"));
+        }
+    }   
+}

b221bdbd5a00d23ce4d18521e1726b4c24e1c187
Rafael León <rafael.leon@atmira.com>
2021-03-22 10:08:51 +0100

    Rot13

---
+++ src/CodeWars/Rot13.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Text;
+
+namespace CodeWars
+{
+    public class Rot13
+    {
+        public static string Translate(string message)
+        {
+            const string part1 = "abcdefghijklm";
+            const string part2 = "nopqrstuvwxyz";
+            var sb = new StringBuilder();
+            foreach (var letter in message)
+            {
+                var indexInPart1 = part1.IndexOf(letter.ToString().ToLower());
+                var indexInPart2 = part2.IndexOf(letter.ToString().ToLower());
+                string translatedChar = default;
+                if (indexInPart1 != -1)
+                {
+                    translatedChar = part2.Substring(indexInPart1, 1);
+                } else if (indexInPart2 != -1)
+                {
+                    translatedChar = part1.Substring(indexInPart2, 1);
+                }
+                else
+                {
+                    translatedChar = letter.ToString();
+                }
+
+                if (Char.IsUpper(letter))
+                {
+                    translatedChar = translatedChar.ToUpper();
+                }
+                sb.Append(translatedChar);
+            }
+
+            return sb.ToString();
+        }
+    }
+}
---
+++ src/CodeWarsTests/Rot13Tests.cs
@@ -0,0 +1,21 @@
+﻿using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class Rot13Tests
+    {
+        [Test, Description("test")]
+        public void testTest()
+        {
+            Assert.AreEqual("grfg", Rot13.Translate("test"), string.Format("Input: test, Expected Output: grfg, Actual Output: {0}", Rot13.Translate("test")));
+        }
+    
+        [Test, Description("Test")]
+        public void TestTest()
+        {
+            Assert.AreEqual("Grfg", Rot13.Translate("Test"), string.Format("Input: Test, Expected Output: Grfg, Actual Output: {0}", Rot13.Translate("Test")));
+        }
+    }
+}
